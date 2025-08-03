b354ce54cecc9ebfb865d29dbf438347f112d688
Nisarga <nisargakshetty@gmail.com>
2025-08-03 21:23:30 +0530

    Update hello.c and add hello.exe

--- hello.c
+++ hello.c
@@ -1,33 +1,34 @@
 #include<stdio.h>
 int  main(){
-    int a[100],n,pos,val;
-    printf("Enter the no of elements in an array:");
-    scanf("%d",&n);
-    printf("Enter the array elements:");
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
+   int a[50],n,pos;
 
-    printf("Enter the position to insert:");
-    scanf("%d",&pos);
-    printf("Enter the value to insert:");
-    scanf("%d",&val);
+   printf("Enter the no of elemnts in an array:");
+   scanf("%d",&n);
 
-    for(int i=n;i>=pos;i--){
-        a[i]=a[i-1];
-    }
-    a[pos-1]=val;
-    n++;
-printf("The new array is:");
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
-    }
+   printf("Enter the array elemnts:");
+   for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+   }
+
+   printf("Enter the position to delete:");
+   scanf("%d",&pos);
+   
+
+if(pos<0 || pos>=n){
+    printf("Invalid position\n");
+}
+
+for(int i=pos-1;i<n;i++){
+    a[i]=a[i+1];
+}
+n--;
+
+printf("The new array:\n");
+for(int i=0;i<n;i++){
+    printf("%d ",a[i]);
+}
+
+
     return 0;
-
-
-
-
-
-
 
 }

79d5d96756ecdcfe4bb1d0841205826489313003
Nisarga <nisargakshetty@gmail.com>
2025-08-03 20:53:50 +0530

    Update hello.c and add hello.exe

--- hello.c
+++ hello.c
@@ -1,4 +1,33 @@
 #include<stdio.h>
-void main(){
-    printf("hello world");
+int  main(){
+    int a[100],n,pos,val;
+    printf("Enter the no of elements in an array:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    printf("Enter the position to insert:");
+    scanf("%d",&pos);
+    printf("Enter the value to insert:");
+    scanf("%d",&val);
+
+    for(int i=n;i>=pos;i--){
+        a[i]=a[i-1];
+    }
+    a[pos-1]=val;
+    n++;
+printf("The new array is:");
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+
+
+
+
+
+
+
 }
